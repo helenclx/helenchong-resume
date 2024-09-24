2519d5ae938c38a758efb197ca7fb2e57360441f
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-09-24 20:42:09 +0800

    Add author global data

---
+++ src/_data/author.js
@@ -0,0 +1,49 @@
+export const author = "Helen Chong";
+export const occupation = "Full-Stack WordPress Developer";
+export const location = "Malaysia";
+export const profilePic = "profilepic.avif";
+export const pronouns = "they/them or she/her";
+export const email = "contact@helenchong.dev";
+export const website = "https://helenchong.dev";
+export const skills = [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "PHP",
+    "WordPress",
+    "Git",
+    "React",
+    "Python",
+    "Graphic Design",
+    "UI Design"
+];
+export const languages = [
+    {
+        name: "Mandarin",
+        level: "native"
+    },
+    {
+        name: "English",
+        level: "fluent"
+    },
+    {
+        name: "Canronese",
+        level: "intermediate"
+    },
+    {
+        name: "Malay",
+        level: "basic"
+    }
+];
+export const socials = [
+    {
+        name: "GitHub",
+        user: "helenclx",
+        url: "https://github.com/helenclx"
+    },
+    {
+        name: "LinkedIn",
+        user: "helenclx",
+        url: "https://www.linkedin.com/in/annaquincey"
+    }
+];

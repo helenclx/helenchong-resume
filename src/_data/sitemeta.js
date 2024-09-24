300ca1510046c878d5d2188eed5a71b8848dc361
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-09-24 20:32:02 +0800

    Add site meta data

---
+++ src/_data/sitemeta.js
@@ -0,0 +1,8 @@
+export const url = "https://helenchong.dev/resume";
+export const canonical = url + "/";
+export const lang = "en";
+export const locale = "en_MY";
+export const author = "Helen Chong";
+export const title = "Résumé of " + author;
+export const themeColor = "#fffbeb";
+export const backgroundColor = "#451a03";

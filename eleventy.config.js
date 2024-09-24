938c2573509fe3c51d69a1cbd4c0e0e7dea0f728
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-09-24 20:24:49 +0800

    Add current year shortcode

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,6 +2,8 @@ export default function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/assets/");
     eleventyConfig.addWatchTarget("./src/assets/");
 
+    eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
     return {
         dir: {
             input: "src"

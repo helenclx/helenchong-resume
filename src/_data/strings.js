163d98dab423a96ac4449b1a226239d02821ef64
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-09-24 21:39:17 +0800

    Add strings data

---
+++ src/_data/strings.js
@@ -0,0 +1,15 @@
+export const titles = {
+    experience: "Work Experience",
+    education: "Education",
+    skills: "Skills",
+    languages: "Languages",
+    projects: "Featured Projects"
+};
+
+export const labels = {
+    email: "Email",
+    telephone: "Telephone",
+    print: "Print Résumé",
+    present: "Present",
+    lastUpdated: "Last updated"
+};

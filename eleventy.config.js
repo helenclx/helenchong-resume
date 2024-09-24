65af4b96d95489283c9883bac2d410da4e25d5f0
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-09-24 21:53:30 +0800

    Set up résumé layout

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,8 +1,18 @@
+import { DateTime } from 'luxon';
+
 export default function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/assets/");
     eleventyConfig.addWatchTarget("./src/assets/");
 
     eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
+    eleventyConfig.addFilter("dateToFormat", (date) => {
+        const dateFormat = "LLLL yyyy";
+        if (typeof date === "object") {
+            return DateTime.fromJSDate(date).toFormat(dateFormat);
+        }
+        return DateTime.fromISO(date, { setZone: true }).toFormat(dateFormat);
+    });
 
     return {
         dir: {

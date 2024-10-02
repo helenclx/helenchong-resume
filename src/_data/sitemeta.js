d0c5fba991dfe5aecba2eb173c9e0590ac9c96a8
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-10-02 19:36:46 +0800

    Add background color

--- src/_data/sitemeta.js
+++ src/_data/sitemeta.js
@@ -3,6 +3,4 @@ export const canonical = url + "/";
 export const lang = "en";
 export const locale = "en_MY";
 export const title = "Résumé of Helen Chong";
-export const themeColor = "#fffbeb";
-export const backgroundColor = "#451a03";
 export const buildTimestamp = new Date();
